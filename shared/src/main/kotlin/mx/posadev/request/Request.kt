56836ea8894fcd56417f53f13eb159da84c4c1cb
Sierisimo Sier <sierisimo@gmail.com>
2017-12-08 22:45:13 -0600

    Add a class that platforms need to implement

--- shared/src/main/kotlin/mx/posadev/request/Request.kt
+++ shared/src/main/kotlin/mx/posadev/request/Request.kt
@@ -1,9 +1,12 @@
 package mx.posadev.request
 
+import mx.posadev.response.Response
 import mx.posadev.version
 
 val HTTP_HEADERS = mapOf(
-        "User-Agent" to "GithubKonzumer-$version",
+        "User-Agent" to "GithubKonzumer-${version}",
         "Accept" to "application/vnd.github.v3+json",
         "Content-Type" to "application/json"
 )
+
+expect class Request

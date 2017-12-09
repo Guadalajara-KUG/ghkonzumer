7a9e187f2f4601e85e1ed80aea0de80f99ada98d
Sierisimo Sier <sierisimo@gmail.com>
2017-12-08 22:54:40 -0600

    Now our api will take a resource and call for it

--- common-jvm/src/main/kotlin/mx/posadev/GithubKonzumer.kt
+++ common-jvm/src/main/kotlin/mx/posadev/GithubKonzumer.kt
@@ -1,9 +1,10 @@
 package mx.posadev
 
+import mx.posadev.request.Resource
 import mx.posadev.response.Response
 
 actual object GithubKonzumer {
-    actual fun api(): Response {
+    actual fun api(endpoint: Resource): Response {
         return object : Response {}
     }
 }
--- shared/src/main/kotlin/mx/posadev/GithubKonzumer.kt
+++ shared/src/main/kotlin/mx/posadev/GithubKonzumer.kt
@@ -1,9 +1,10 @@
 package mx.posadev
 
+import mx.posadev.request.Resource
 import mx.posadev.response.Response
 
 val version = "0.0.1"
 
 expect object GithubKonzumer {
-    fun api(): Response
+    fun api(endpoint: Resource): Response
 }

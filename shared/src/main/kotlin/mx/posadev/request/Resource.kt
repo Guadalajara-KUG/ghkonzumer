436c75b95559888f7ef896ebd5aa7d3cb3c1a9bb
Sierisimo Sier <sierisimo@gmail.com>
2017-12-09 00:29:06 -0600

    Let's request a User

--- shared/src/main/kotlin/mx/posadev/request/Resource.kt
+++ shared/src/main/kotlin/mx/posadev/request/Resource.kt
@@ -8,4 +8,8 @@ sealed class Resource(val endpoint: String) {
 
 object Zen : Resource("/$zen")
 
+class User(username: String, fetchRepos: Boolean = false) : Resource("/$users/$username${if (fetchRepos) "/$repos" else ""}")
+
 private val zen = "zen"
+private val users = "users"
+private val repos = "repos"
